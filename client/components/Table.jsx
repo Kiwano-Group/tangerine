430e76377e140ef533426892b1e5c44e0f06a635
Brian <118695080+B3YONDLIF3@users.noreply.github.com>
2023-09-28 16:53:36 -0400

    Fixed Merge between herman/brian

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -7,57 +7,121 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SecretButton from "./SecretButton.jsx";
 
 const Table = () => {
-  const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [rowsPerPage] = useState(10);
-  const [filter, filterOn] = useState(false);
-  const [filteredData, setFilteredData] = useState([]); // New state for filtered data
+const [data, setData] = useState([]);
+const [currentPage, setCurrentPage] = useState(1);
+const [rowsPerPage] = useState(10);
 
-  const getTableFunc = () => {
-    fetch("/api/table")
-      .then(response => response.json())
-      .then(fetchedData => setData(fetchedData));
+const [filter, filterOn] = useState(false);
+const [filteredData, setFilteredData] = useState([]); 
+
+const [employeeDropdownStates, setEmployeeDropdownStates] = useState({});
+const [dropdownOpen, setDropdownOpen] = useState(false);
+const [employeeFormSubmitted, setEmployeeFormSubmitted] = useState({});
+const [employeeDropdownOpen, setEmployeeDropdownOpen] = useState({});
+const [employeeEdit, setEmployeeEdit] = useState({});
+const [formSubmitted, setFormSubmitted] = useState(false);
+const [formData, setFormData] = useState({
+  end_date: "",
+  obTime: "",
+});
+const date = new Date();
+let month = ("0" + (date.getMonth() + 1)).slice(-2);
+let day = ("0" + date.getDate()).slice(-2);
+const currentDate =  date.getFullYear().toString() + '-' + month + '-' + day;
+
+const getTableFunc = () => {
+  fetch("/api/table")
+  .then(response => response.json())
+  .then(fetchedData => {
+      const updatedData = fetchedData.map(employee => ({
+      ...employee,
+      formSubmitted: false,
+      dropdownOpen: false
+      }));
+      setData(updatedData);
+      const initialEmployeeFormSubmitted = {};
+      const initialEmployeeDropdownOpen = {};
+      fetchedData.forEach(employee => {
+      initialEmployeeFormSubmitted[employee.employee_id] = false;
+      initialEmployeeDropdownOpen[employee.employee_id] = false;
+      });
+      setEmployeeFormSubmitted(initialEmployeeFormSubmitted);
+      setEmployeeDropdownOpen(initialEmployeeDropdownOpen)
+  });
+}
+
+useEffect(getTableFunc, []);
+
+const lastRowIndex = currentPage * rowsPerPage;
+const firstRowIndex = lastRowIndex - rowsPerPage;
+const currentData = data.slice(firstRowIndex, lastRowIndex);
+
+const totalPages = Math.ceil(data.length / rowsPerPage);
+
+const formatDate = (databaseDate) => {
+  const date = new Date(databaseDate);
+  return date.toISOString().split('T')[0];
+};
+
+const toggleDropdown = (employeeId) => {
+  setEmployeeDropdownOpen(prevState => ({
+      ...prevState,
+      [employeeId]: !prevState[employeeId]
+  }));
+}
+const editHandle = async (employeeId) => {
+  try{
+  } catch (err){
+      console.log('Error editing employee')
   }
+};
+const clickHandle = async (employeeId) => {
+  console.log(employeeId);
+  try {
+      setEmployeeFormSubmitted(prevEmployeeFormSubmitted => ({
+          ...prevEmployeeFormSubmitted,
+          [employeeId]: true,
+        }));
+      const res = await fetch(`/api/delete/${employeeId}`, {
+          method: 'DELETE',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              end_date: formData.end_date,
+              obTime: formData.obTime,
+              employeeId: employeeId,
+          }),
+      });
+      console.log('true')
+      setFormSubmitted(true);
+      //rerenders table
+      getTableFunc();
+  } catch (err) {
+      console.log('error deleting employee');
+  }
+}
+const handleFormChange = (e) => {
+  e.preventDefault();
+  setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+  });
+}
+const handleSubmit = (event) => {
+  event.preventDefault();
+}
 
-  useEffect(getTableFunc, []);
-
-  const lastRowIndex = currentPage * rowsPerPage;
-  const firstRowIndex = lastRowIndex - rowsPerPage;
-  const currentData = data.slice(firstRowIndex, lastRowIndex);
-
-  const totalPages = Math.ceil(data.length / rowsPerPage);
-
-  const formatDate = (databaseDate) => {
-    try {
-      if (!databaseDate) {
-        return ''; // Handle cases where databaseDate is empty or undefined
-      }
-  
-      const date = new Date(databaseDate);
-      if (isNaN(date.getTime())) {
-        return 'Invalid Date'; // Handle invalid date strings
-      }
-  
-      return date.toISOString().split('T')[0];
-    } catch (error) {
-      console.error('Error formatting date:', error);
-      return 'Error';
-    }
-  };
-  
-
-  const nameFilter = async () => {
+const nameFilter = async () => {
     try {
       const res = await fetch('/api/filterFirstName')
         .then(res => res.json());
 
-      // Update the filteredData state with the filtered results
       setFilteredData(res);
     } catch (err) {
       console.log('error filter by first name', err);
     }
   }
-
   return (
     <div className="container">
       <div className='tableHead'>
@@ -71,7 +135,6 @@ const Table = () => {
       </div>
       <div className="tableBody">
         {filteredData.length > 0 ? filteredData.map((employee) => (
-          // Map over the filteredData if it's available
           <Accordion key={employee.employee_id}>
                   <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
@@ -99,7 +162,6 @@ const Table = () => {
         </AccordionDetails>
           </Accordion>
         )) : currentData.map((employee) => (
-          // Map over currentData if filteredData is empty
           <Accordion key={employee.employee_id}>
                   <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
@@ -116,30 +178,61 @@ const Table = () => {
         </AccordionSummary>
         <AccordionDetails>
         <Typography>
-            <div className="employeeDetail">Salary: {employee.salary}</div>
-            <div className="employeeDetail">Birthday: {employee.birthday}</div>
+        <div className="employeeDetail">Salary: {employee.salary}</div>
+            <div className="employeeDetail">Birthday: {formatDate(employee.birthday)}</div>
             <div className="employeeDetail">Email: {employee.email}</div>
             <div className="employeeDetail">Phone Number: {employee.phone_number}</div>
             <br />
-            <button>Gettem Outta Here</button>
-            <button>Change This Person</button>
+            <div className="flex-center">
+            {employeeFormSubmitted[employee.employee_id] ? (<div className="Button">Employee offboard scheduled</div>) : (
+                <div className="dropdown">
+                    <button onClick={() => toggleDropdown(employee.employee_id)}>
+                        {employeeDropdownOpen[employee.employee_id] ? "Go Back" : "Offboard"}
+                    </button>
+                                {employeeDropdownOpen[employee.employee_id] && (
+                            <div className="dropdown-content">
+                            <form onSubmit={handleSubmit}>
+                                <ul>
+                                    <li>
+                                        <label htmlFor="end_date">Offboard Date </label>
+                                        <input type="date" id="end" name="end_date" min={currentDate} value={formData.end_date} onChange={handleFormChange} />
+                                    </li>
+                                    <li>
+                                        <label htmlFor="end_time">Offboard Time </label>
+                                        <input type="time" id="obTime" name="obTime" min="06:00" max="24:00" required value={formData.obTime} onChange={handleFormChange} />
+                                    </li>
+                                    <li>
+                                        <button type="submit" onClick={() => clickHandle(employee.employee_id)}>Confirm Offboarding</button>
+                                    </li>
+                                </ul>
+                            </form>
+                        </div>
+                        )}
+                    {employeeDropdownOpen[employee.employee_id] ? null: <button className="Button">Edit Info</button>}
+                </div>
+                )}
+        </div>
         </Typography>
         </AccordionDetails>
           </Accordion>
         ))}
       </div>
-      <div className="flex-center">
-        <button className="button-style"
-          onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-          disabled={currentPage === 1}>Previous
-        </button>
-        <button className="button-style"
-          onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
-          disabled={currentPage === totalPages}>Next
-        </button>
-      </div>
-    </div>
-  );
+      
+      <br></br>
+            <div className="flex-center">
+                <button className="button-style"
+                    onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+                    disabled={currentPage === 1}>
+                    Previous
+                </button>
+                <button className="button-style"
+                    onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+                    disabled={currentPage === totalPages}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Table;
